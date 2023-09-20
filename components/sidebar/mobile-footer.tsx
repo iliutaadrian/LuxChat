5c92ad908ec335bd906c7b1306683c5c03dea308
iliutaadrian <iliuta_rocky@yahoo.com>
2023-09-20 14:39:16 +0300

    fix build errors

--- components/sidebar/mobile-footer.tsx
+++ components/sidebar/mobile-footer.tsx
@@ -16,7 +16,7 @@ export const MobileFooter = () => {
                 <ul role={'list'} className={'flex flex-row w-full justify-evenly'}>
                     {routes.map((route) => {
                         return (
-                            <li key={route.label} onClick={route?.onClick}>
+                            <li key={route.label}>
                                 <Link href={route.href} className={cn("mx-2 my-5 gap-x-3 rounded-xl cursor-pointer w-12 h-12 flex flex-col align-middle justify-center hover:bg-muted", route.active && `bg-muted-foreground/30`)}>
                                     <route.icon className={'w-8 h-8 mx-auto p-1'} />
                                     <span className={'sr-only'}>{route.label}</span>
