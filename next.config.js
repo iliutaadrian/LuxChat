f570e50f5340b0535ef38eebceaf1900796a1e94
iliutaadrian <iliuta_rocky@yahoo.com>
2023-09-25 18:13:42 +0300

    PWA

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
 const nextConfig = {
+  ...withPWA({
+    dest: 'public',
+    register: true,
+    skipWaiting: truegti
+  }),
   images: {
     domains: ['res.cloudinary.com'],
   }
